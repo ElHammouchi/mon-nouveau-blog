ebe02cea030e9d18ad7bce4f187deae3ee0cfff1
Pr. El Hammouchi <el-hammouchi@simple.com>
2021-09-19 15:39:02 +0200

    accounts, boards, forms, gravatar

--- algebra/settings.py
+++ algebra/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
+SECRET_KEY = '9=$05kqu9snzb=2lqygrw!%o(xk893ezl7fxt_t5+ik0e0wwgi'
+# SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
