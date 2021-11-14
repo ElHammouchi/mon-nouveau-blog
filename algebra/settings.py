0b59bbfedcb66fdcfd86eb14fcec1b79256b06d7
ElHammouchi <pr.elhammouchi@gmail.com>
2021-11-14 20:59:49 +0100

    PAramétrage des mails

--- algebra/settings.py
+++ algebra/settings.py
@@ -138,3 +138,13 @@ CKEDITOR_CONFIGS = {
         'toolbar': None,
     },
 }
+
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = 'M. El Hammouchi <noreply@lanno.org>'
+EMAIL_SUBJECT_PREFIX = '[M. El Hammouchi] '
